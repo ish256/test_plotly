6c6b5f464ff4573cf93e10df6e1a1522825e45a7
Ismael Xique <ixique@umich.edu>
2018-12-17 22:09:14 -0500

    working app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Plot from "react-plotly.js";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
+class Rangecomponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <input
+          type="range"
+          id="cowbell"
+          name="cowbell"
+          min="0"
+          max="100"
+          value={this.state.value}
+          onChange={this.handleChange}
+          step="10"
+        />
+        <label for="volume">Volume = {this.state.value}</label>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Rangecomponent />
+        <Rangecomponent />
+        <Rangecomponent />
+        <div>
+          <Plot
+            data={[
+              {
+                x: [1, 3, 3],
+                y: [2, 6, 3],
+                type: "scatter",
+                mode: "lines+points",
+                marker: { color: "red" }
+              },
+              { type: "bar", x: [1, 2, 3], y: [2, 5, 3] }
+            ]}
+            layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
+          />
+        </div>
       </div>
     );
   }
