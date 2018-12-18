4029eb9ff1accc7cd74795b9ea9cd01458ef5d24
Ismael Xique <ixique@umich.edu>
2018-12-17 23:15:28 -0500

    add working slider

--- src/App.js
+++ src/App.js
@@ -3,55 +3,43 @@ import Plot from "react-plotly.js";
 import logo from "./logo.svg";
 import "./App.css";
 
-class Rangecomponent extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = {
+      value: "5",
+      data: [{ type: "bar", x: [1, 2, 3], y: [2, 5, 3] }]
+    };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
     this.setState({ value: event.target.value });
+    this.setState({
+      data: [{ type: "bar", x: [1, 2, 3], y: [2, 5, event.target.value] }]
+    });
   }
 
   render() {
     return (
       <div>
-        <input
-          type="range"
-          id="cowbell"
-          name="cowbell"
-          min="0"
-          max="100"
-          value={this.state.value}
-          onChange={this.handleChange}
-          step="10"
-        />
-        <label for="volume">Volume = {this.state.value}</label>
-      </div>
-    );
-  }
-}
+        <div>
+          <input
+            type="range"
+            id="cowbell"
+            name="cowbell"
+            min="0"
+            max="10"
+            value={this.state.value}
+            onChange={this.handleChange}
+            step="1"
+          />
+          <label for="volume">Volume = {this.state.value}</label>
+        </div>
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Rangecomponent />
-        <Rangecomponent />
-        <Rangecomponent />
         <div>
           <Plot
-            data={[
-              {
-                x: [1, 3, 3],
-                y: [2, 6, 3],
-                type: "scatter",
-                mode: "lines+points",
-                marker: { color: "red" }
-              },
-              { type: "bar", x: [1, 2, 3], y: [2, 5, 3] }
-            ]}
+            data={this.state.data}
             layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
           />
         </div>
